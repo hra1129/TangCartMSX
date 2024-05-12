3f5aec62f4aed86c071850229b988ed3a3da3d53
HRA! <hra@mvj.biglobe.ne.jp>
2024-05-13 06:25:29 +0900

    Updated

--- RTL/TangcartMSX/src/psram/ip_psram.v
+++ RTL/TangcartMSX/src/psram/ip_psram.v
@@ -74,33 +74,33 @@ module ip_psram (
 	localparam	[3:0]	WAIT_CNT		= 14;
 
 	PSRAM_Memory_Interface_2CH_Top u_gowin_psram_if_2ch (
-		.clk				( clk						), //input clk
-		.rst_n				( n_reset					), //input rst_n
-		.O_psram_ck			( O_psram_ck				), //output [1:0] O_psram_ck
-		.O_psram_ck_n		( O_psram_ck_n				), //output [1:0] O_psram_ck_n
-		.IO_psram_rwds		( IO_psram_rwds				), //inout [1:0] IO_psram_rwds
-		.O_psram_reset_n	( O_psram_reset_n			), //output [1:0] O_psram_reset_n
-		.IO_psram_dq		( IO_psram_dq				), //inout [15:0] IO_psram_dq
-		.O_psram_cs_n		( O_psram_cs_n				), //output [1:0] O_psram_cs_n
-		.init_calib0		( w_init_complete0			), //output init_calib0
-		.init_calib1		( w_init_complete1			), //output init_calib1
-		.clk_out			( 							), //output clk_out
-		.cmd0				( wr0						), //input cmd0
-		.cmd1				( wr1						), //input cmd1
-		.cmd_en0			( w_cmd0_en					), //input cmd_en0
-		.cmd_en1			( w_cmd1_en					), //input cmd_en1
-		.addr0				( { 1'b0, address0[21:2] }	), //input [20:0] addr0
-		.addr1				( { 1'b0, address1[21:2] }	), //input [20:0] addr1
-		.wr_data0			( wr_data0_i				), //input [31:0] wr_data0
-		.wr_data1			( wr_data1_i				), //input [31:0] wr_data1
-		.rd_data0			( w_rd_data0				), //output [31:0] rd_data0
-		.rd_data1			( w_rd_data1				), //output [31:0] rd_data1
-		.rd_data_valid0		( w_rdata0_en				), //output rd_data_valid0
-		.rd_data_valid1		( w_rdata1_en				), //output rd_data_valid1
-		.data_mask0			( w_wr_mask0				), //input [3:0] data_mask0
-		.data_mask1			( w_wr_mask1				), //input [3:0] data_mask1
-		.memory_clk			( mem_clk					), //input memory_clk
-		.pll_lock			( lock						)  //input pll_lock
+		.clk				( clk					), //input clk
+		.rst_n				( n_reset				), //input rst_n
+		.O_psram_ck			( O_psram_ck			), //output [1:0] O_psram_ck
+		.O_psram_ck_n		( O_psram_ck_n			), //output [1:0] O_psram_ck_n
+		.IO_psram_rwds		( IO_psram_rwds			), //inout [1:0] IO_psram_rwds
+		.O_psram_reset_n	( O_psram_reset_n		), //output [1:0] O_psram_reset_n
+		.IO_psram_dq		( IO_psram_dq			), //inout [15:0] IO_psram_dq
+		.O_psram_cs_n		( O_psram_cs_n			), //output [1:0] O_psram_cs_n
+		.init_calib0		( w_init_complete0		), //output init_calib0
+		.init_calib1		( w_init_complete1		), //output init_calib1
+		.clk_out			( 						), //output clk_out
+		.cmd0				( wr0					), //input cmd0
+		.cmd1				( wr1					), //input cmd1
+		.cmd_en0			( w_cmd0_en				), //input cmd_en0
+		.cmd_en1			( w_cmd1_en				), //input cmd_en1
+		.addr0				( address0[21:1]		), //input [20:0] addr0
+		.addr1				( address1[21:1]		), //input [20:0] addr1
+		.wr_data0			( wr_data0_i			), //input [31:0] wr_data0
+		.wr_data1			( wr_data1_i			), //input [31:0] wr_data1
+		.rd_data0			( w_rd_data0			), //output [31:0] rd_data0
+		.rd_data1			( w_rd_data1			), //output [31:0] rd_data1
+		.rd_data_valid0		( w_rdata0_en			), //output rd_data_valid0
+		.rd_data_valid1		( w_rdata1_en			), //output rd_data_valid1
+		.data_mask0			( w_wr_mask0			), //input [3:0] data_mask0
+		.data_mask1			( w_wr_mask1			), //input [3:0] data_mask1
+		.memory_clk			( mem_clk				), //input memory_clk
+		.pll_lock			( lock					)  //input pll_lock
 	);
 
 	// --------------------------------------------------------------------
@@ -152,15 +152,13 @@ module ip_psram (
 	assign w_wr_data1	= { wdata1, wdata1, wdata1, wdata1 };
 
 	function [3:0] func_mask_decode(
-		input	[1:0]	address,
+		input			address,
 		input			cmd_en
 	);
 		if( cmd_en ) begin
 			case( address )
-			2'b00:		func_mask_decode = 4'b1110;
-			2'b01:		func_mask_decode = 4'b1101;
-			2'b10:		func_mask_decode = 4'b1011;
-			2'b11:		func_mask_decode = 4'b0111;
+			1'b0:		func_mask_decode = 4'b1110;
+			1'b1:		func_mask_decode = 4'b1101;
 			default:	func_mask_decode = 4'b1110;
 			endcase
 		end
@@ -169,21 +167,19 @@ module ip_psram (
 		end
 	endfunction
 
-	assign w_wr_mask0	= func_mask_decode( address0[1:0], w_cmd0_en );
-	assign w_wr_mask1	= func_mask_decode( address1[1:0], w_cmd1_en );
+	assign w_wr_mask0	= func_mask_decode( address0[0], w_cmd0_en );
+	assign w_wr_mask1	= func_mask_decode( address1[0], w_cmd1_en );
 
 	// --------------------------------------------------------------------
 	//	Read data signal
 	// --------------------------------------------------------------------
 	function [7:0] func_rd_data(
-		input	[1:0]	address,
+		input			address,
 		input	[31:0]	rd_data
 	);
 		case( address )
-		2'b00:		func_rd_data = rd_data[ 7: 0];
-		2'b01:		func_rd_data = rd_data[15: 8];
-		2'b10:		func_rd_data = rd_data[23:16];
-		2'b11:		func_rd_data = rd_data[31:24];
+		1'b0:		func_rd_data = rd_data[ 7: 0];
+		1'b1:		func_rd_data = rd_data[15: 8];
 		default:	func_rd_data = rd_data[ 7: 0];
 		endcase
 	endfunction
@@ -199,7 +195,7 @@ module ip_psram (
 		end
 		else if( !ff_rd_data0_en && w_rdata0_en ) begin
 			ff_rd_data0_en	<= 1'b1;
-			ff_rd_data0		<= func_rd_data( address0[1:0], w_rd_data0 );
+			ff_rd_data0		<= func_rd_data( address0[0], w_rd_data0 );
 		end
 		else begin
 			//	hold
@@ -217,7 +213,7 @@ module ip_psram (
 		end
 		else if( !ff_rd_data1_en && w_rdata1_en ) begin
 			ff_rd_data1_en	<= 1'b1;
-			ff_rd_data1		<= func_rd_data( address1[1:0], w_rd_data1 );
+			ff_rd_data1		<= func_rd_data( address1[0], w_rd_data1 );
 		end
 		else begin
 			//	hold
