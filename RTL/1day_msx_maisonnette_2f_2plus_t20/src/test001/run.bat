629fe980a917b609cd212c34169ffe36d3f85bec
HRA! <hra@mvj.biglobe.ne.jp>
2024-10-30 21:30:00 +0900

    Updated
    vdp_spinforam の verilog化

--- RTL/1day_msx_maisonnette_2f_2plus_t20/src/test001/run.bat
+++ RTL/1day_msx_maisonnette_2f_2plus_t20/src/test001/run.bat
@@ -9,24 +9,24 @@ vlog ..\sdram\ip_sdram.v
 rem vlog ..\sdram\ip_sdram_dummy.v
 rem vlog ..\sdram\ip_sdram_dummy2.v
 
-vlog ..\v9958clone\vdp_ram256.v
-vcom ..\v9958clone\vdp_package.vhd
-vlog ..\v9958clone\vdp_colordec.v
-vcom ..\v9958clone\vdp_command.vhd
-vlog ..\v9958clone\vdp_doublebuf.v
-vlog ..\v9958clone\vdp_graphic123m.v
-vcom ..\v9958clone\vdp_graphic4567.vhd
-vcom ..\v9958clone\vdp_hvcounter.vhd
-vlog ..\v9958clone\vdp_interrupt.v
-vlog ..\v9958clone\vdp_linebuf.v
-vlog ..\v9958clone\vdp_register.v
-vcom ..\v9958clone\vdp_spinforam.vhd
-vcom ..\v9958clone\vdp_sprite.vhd
-vlog ..\v9958clone\vdp_ssg.v
-vcom ..\v9958clone\vdp_text12.vhd
-vlog ..\v9958clone\vdp_lcd.v
-vlog ..\v9958clone\vdp_wait_control.v
-vcom ..\v9958clone\vdp.vhd
+vlog ..\v9958\vdp_ram256.v
+vcom ..\v9958\vdp_package.vhd
+vlog ..\v9958\vdp_colordec.v
+vcom ..\v9958\vdp_command.vhd
+vlog ..\v9958\vdp_doublebuf.v
+vlog ..\v9958\vdp_graphic123m.v
+vcom ..\v9958\vdp_graphic4567.vhd
+vcom ..\v9958\vdp_hvcounter.vhd
+vlog ..\v9958\vdp_interrupt.v
+vlog ..\v9958\vdp_linebuf.v
+vlog ..\v9958\vdp_register.v
+vlog ..\v9958\vdp_spinforam.v
+vcom ..\v9958\vdp_sprite.vhd
+vlog ..\v9958\vdp_ssg.v
+vcom ..\v9958\vdp_text12.vhd
+vlog ..\v9958\vdp_lcd.v
+vlog ..\v9958\vdp_wait_control.v
+vcom ..\v9958\vdp.vhd
 vlog gowin_pll_dummy.v
 vlog ..\tang20cart_msx.v
 vlog tb.sv
