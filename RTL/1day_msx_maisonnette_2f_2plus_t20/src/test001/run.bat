a2bc4478b8ceadd22897ba29b4419bded0a0940b
HRA! <hra@mvj.biglobe.ne.jp>
2024-11-03 05:53:37 +0900

    Updated
    ディレクトリ構成を変更

--- RTL/1day_msx_maisonnette_2f_2plus_t20/src/test001/run.bat
+++ RTL/1day_msx_maisonnette_2f_2plus_t20/src/test001/run.bat
@@ -5,27 +5,27 @@ vlog ..\debugger\vram_image_rom.v
 
 vlog MT48LC2M32B2.v
 
-vlog ..\sdram\ip_sdram.v
-rem vlog ..\sdram\ip_sdram_dummy.v
-rem vlog ..\sdram\ip_sdram_dummy2.v
+vlog ..\..\..\modules\sdram\ip_sdram.v
+rem vlog ..\..\..\modules\sdram\ip_sdram_dummy.v
+rem vlog ..\..\..\modules\sdram\ip_sdram_dummy2.v
 
-vlog ..\v9958\vdp_ram256.v
-vlog ..\v9958\vdp_colordec.v
-vlog ..\v9958\vdp_command.v
-vlog ..\v9958\vdp_doublebuf.v
-vlog ..\v9958\vdp_graphic123m.v
-vlog ..\v9958\vdp_graphic4567.v
-vlog ..\v9958\vdp_interrupt.v
-vlog ..\v9958\vdp_linebuf.v
-vlog ..\v9958\vdp_register.v
-vlog ..\v9958\vdp_spinforam.v
-vlog ..\v9958\vdp_sprite.v
-vlog ..\v9958\vdp_ssg.v
-vlog ..\v9958\vdp_text12.v
-vlog ..\v9958\vdp_lcd.v
-vlog ..\v9958\vdp_wait_control.v
-vlog ..\v9958\vdp.v
-vlog ..\v9958\vdp_inst.v
+vlog ..\..\..\modules\v9958\vdp_ram256.v
+vlog ..\..\..\modules\v9958\vdp_colordec.v
+vlog ..\..\..\modules\v9958\vdp_command.v
+vlog ..\..\..\modules\v9958\vdp_doublebuf.v
+vlog ..\..\..\modules\v9958\vdp_graphic123m.v
+vlog ..\..\..\modules\v9958\vdp_graphic4567.v
+vlog ..\..\..\modules\v9958\vdp_interrupt.v
+vlog ..\..\..\modules\v9958\vdp_linebuf.v
+vlog ..\..\..\modules\v9958\vdp_register.v
+vlog ..\..\..\modules\v9958\vdp_spinforam.v
+vlog ..\..\..\modules\v9958\vdp_sprite.v
+vlog ..\..\..\modules\v9958\vdp_ssg.v
+vlog ..\..\..\modules\v9958\vdp_text12.v
+vlog ..\..\..\modules\v9958\vdp_lcd.v
+vlog ..\..\..\modules\v9958\vdp_wait_control.v
+vlog ..\..\..\modules\v9958\vdp.v
+vlog ..\..\..\modules\v9958\vdp_inst.v
 vlog gowin_pll_dummy.v
 vlog ..\tang20cart_msx.v
 vlog tb.sv
