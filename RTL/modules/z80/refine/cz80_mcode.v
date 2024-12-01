385d50c7bad12ec03ae7fcbb87cb03e50618a6b4
HRA! <hra@mvj.biglobe.ne.jp>
2024-12-01 21:38:47 +0900

    Update cz80_mcode.v

--- RTL/modules/z80/refine/cz80_mcode.v
+++ RTL/modules/z80/refine/cz80_mcode.v
@@ -716,6 +716,8 @@ module cz80_mcode (
 			default:
 				func_inc_wz = 1'b0;
 			endcase
+		2'b01:
+			func_inc_wz = 1'b0;
 		default:
 			// --------------------------------------------------------------------
 			//	ED prefixed instructions
@@ -1436,7 +1438,7 @@ module cz80_mcode (
 				if( mcycle == 3'd4 ) begin
 					func_set_busa_to = 4'd5;	// l
 				end
-				else if( mcycle == 3'd4 ) begin
+				else if( mcycle == 3'd5 ) begin
 					func_set_busa_to = 4'd4;	// h
 				end
 				else begin
@@ -2023,7 +2025,45 @@ module cz80_mcode (
 				func_set_addr_to = axy;
 			end
 			else begin
-				func_set_addr_to = anone;
+				case( irb )
+				8'h06, 8'h16, 8'h26, 8'h36, 8'h0E, 8'h1E, 8'h2E, 8'h3E,
+				8'h46, 8'h56, 8'h66, 8'h76, 8'h4E, 8'h5E, 8'h6E, 8'h7E,
+				8'h86, 8'h96, 8'hA6, 8'hB6, 8'h8E, 8'h9E, 8'hAE, 8'hBE,
+				8'hC6, 8'hD6, 8'hE6, 8'hF6, 8'hCE, 8'hDE, 8'hEE, 8'hFE:
+					func_set_addr_to = axy;
+				8'h40, 8'h41, 8'h42, 8'h43, 8'h44, 8'h45, 8'h47,
+				8'h48, 8'h49, 8'h4A, 8'h4B, 8'h4C, 8'h4D, 8'h4F,
+				8'h50, 8'h51, 8'h52, 8'h53, 8'h54, 8'h55, 8'h57,
+				8'h58, 8'h59, 8'h5A, 8'h5B, 8'h5C, 8'h5D, 8'h5F,
+				8'h60, 8'h61, 8'h62, 8'h63, 8'h64, 8'h65, 8'h67,
+				8'h68, 8'h69, 8'h6A, 8'h6B, 8'h6C, 8'h6D, 8'h6F,
+				8'h70, 8'h71, 8'h72, 8'h73, 8'h74, 8'h75, 8'h77,
+				8'h78, 8'h79, 8'h7A, 8'h7B, 8'h7C, 8'h7D, 8'h7F,
+				8'h80, 8'h81, 8'h82, 8'h83, 8'h84, 8'h85, 8'h87,
+				8'h88, 8'h89, 8'h8A, 8'h8B, 8'h8C, 8'h8D, 8'h8F,
+				8'h90, 8'h91, 8'h92, 8'h93, 8'h94, 8'h95, 8'h97,
+				8'h98, 8'h99, 8'h9A, 8'h9B, 8'h9C, 8'h9D, 8'h9F,
+				8'hA0, 8'hA1, 8'hA2, 8'hA3, 8'hA4, 8'hA5, 8'hA7,
+				8'hA8, 8'hA9, 8'hAA, 8'hAB, 8'hAC, 8'hAD, 8'hAF,
+				8'hB0, 8'hB1, 8'hB2, 8'hB3, 8'hB4, 8'hB5, 8'hB7,
+				8'hB8, 8'hB9, 8'hBA, 8'hBB, 8'hBC, 8'hBD, 8'hBF,
+				8'hC0, 8'hC1, 8'hC2, 8'hC3, 8'hC4, 8'hC5, 8'hC7,
+				8'hC8, 8'hC9, 8'hCA, 8'hCB, 8'hCC, 8'hCD, 8'hCF,
+				8'hD0, 8'hD1, 8'hD2, 8'hD3, 8'hD4, 8'hD5, 8'hD7,
+				8'hD8, 8'hD9, 8'hDA, 8'hDB, 8'hDC, 8'hDD, 8'hDF,
+				8'hE0, 8'hE1, 8'hE2, 8'hE3, 8'hE4, 8'hE5, 8'hE7,
+				8'hE8, 8'hE9, 8'hEA, 8'hEB, 8'hEC, 8'hED, 8'hEF,
+				8'hF0, 8'hF1, 8'hF2, 8'hF3, 8'hF4, 8'hF5, 8'hF7,
+				8'hF8, 8'hF9, 8'hFA, 8'hFB, 8'hFC, 8'hFD, 8'hFF:
+					if( xy_state != 2'b00 ) begin
+						func_set_addr_to = axy;
+					end
+					else begin
+						func_set_addr_to = anone;
+					end
+				default:
+					func_set_addr_to = anone;
+				endcase
 			end
 		end
 		else begin
@@ -2123,13 +2163,13 @@ module cz80_mcode (
 				8'hB0, 8'hB1, 8'hB2, 8'hB3, 8'hB4, 8'hB5, 8'hB7, 
 				8'hB8, 8'hB9, 8'hBA, 8'hBB, 8'hBC, 8'hBD, 8'hBF:
 					if( xy_state != 2'b00 ) begin
-						func_set_addr_to = ( mcycle == 3'd1 || mcycle == 3'd2 || mcycle == 3'd7 ) ? axy: anone;
+						func_set_addr_to = ( mcycle == 3'd1 || mcycle == 3'd2 ) ? axy: anone;
 					end
 					else begin
 						func_set_addr_to = anone;
 					end
 				8'h06, 8'h16, 8'h26, 8'h36, 8'h0E, 8'h1E, 8'h2E, 8'h3E:
-					func_set_addr_to = ( mcycle == 3'd1 || mcycle == 3'd2 || mcycle == 3'd7 ) ? axy: anone;
+					func_set_addr_to = ( mcycle == 3'd1 || mcycle == 3'd2 ) ? axy: anone;
 				8'h40, 8'h41, 8'h42, 8'h43, 8'h44, 8'h45, 8'h47, 
 				8'h48, 8'h49, 8'h4A, 8'h4B, 8'h4C, 8'h4D, 8'h4F, 
 				8'h50, 8'h51, 8'h52, 8'h53, 8'h54, 8'h55, 8'h57, 
@@ -2139,16 +2179,16 @@ module cz80_mcode (
 				8'h70, 8'h71, 8'h72, 8'h73, 8'h74, 8'h75, 8'h77, 
 				8'h78, 8'h79, 8'h7A, 8'h7B, 8'h7C, 8'h7D, 8'h7F:
 					if( xy_state != 2'b00 ) begin
-						func_set_addr_to = ( mcycle == 3'd1 || mcycle == 3'd7 ) ? axy: anone;
+						func_set_addr_to = ( mcycle == 3'd1 ) ? axy: anone;
 					end
 					else begin
 						func_set_addr_to = anone;
 					end
 				8'h46, 8'h4E, 8'h56, 8'h5E, 8'h66, 8'h6E, 8'h76, 8'h7E:
-					func_set_addr_to = ( mcycle == 3'd1 || mcycle == 3'd7 ) ? axy: anone;
+					func_set_addr_to = ( mcycle == 3'd1 ) ? axy: anone;
 				8'hC6, 8'hCE, 8'hD6, 8'hDE, 8'hE6, 8'hEE, 8'hF6, 8'hFE,
 				8'h86, 8'h8E, 8'h96, 8'h9E, 8'hA6, 8'hAE, 8'hB6, 8'hBE:
-					func_set_addr_to = ( mcycle == 3'd1 || mcycle == 3'd2 || mcycle == 3'd7 ) ? axy: anone;
+					func_set_addr_to = ( mcycle == 3'd1 || mcycle == 3'd2 ) ? axy: anone;
 				default:
 					func_set_addr_to = anone;
 				endcase
@@ -2220,17 +2260,14 @@ module cz80_mcode (
 			//  cb prefixed instructions
 			// --------------------------------------------------------------------
 			2'b01:
+				func_noread = 1'b0;
+			default:
 				case( irb )
 				8'hA0, 8'hA8, 8'hB0, 8'hB8, 8'hA2, 8'hAA, 8'hB2, 8'hBA,
 				8'hA3, 8'hAB, 8'hB3, 8'hBB:
 					func_noread = ( mcycle == 3'd4 );
 				8'hA1, 8'hA9, 8'hB1, 8'hB9:
 					func_noread = ( mcycle == 3'd3 || mcycle == 3'd4 );
-				default:
-					func_noread = 1'b0;
-				endcase
-			default:
-				case( irb )
 				8'h42, 8'h52, 8'h62, 8'h72, 8'h4A, 8'h5A, 8'h6A, 8'h7A:
 					func_noread = ( mcycle == 3'd2 || mcycle == 3'd3 );
 				8'h6F:
